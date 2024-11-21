b97a8b4ff4883887959415b0c0957d6efcefdf54
RomwelFeliciano <romfeliciano18@gmail.com>
2024-11-21 17:25:27 +0800

    laravel-practice

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+
+
+        // dump(Idea::all());
+
+        return view('welcome', [
+            'ideas' => Idea::orderBy('created_at', 'DESC')->paginate(5),
+        ]);
+    }
+}
---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+    //
+    public function store()
+    {
+        request()->validate([
+            'idea' => 'required|min:3|max:240',
+        ]);
+        $content = request()->get('idea', '');
+
+        Idea::create([
+            'content' => $content,
+        ]);
+
+        return redirect()->route(route: 'dashboard')->with('success', 'Idea was created successfully!');
+    }
+
+    public function destroy($id)
+    {
+        Idea::where('id', $id)->firstOrFail()->delete();
+
+        return redirect()->route('dashboard')->with('success', 'Deleted Idea');
+    }
+}
