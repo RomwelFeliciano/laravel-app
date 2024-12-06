3e98848cefa57ab5af733af679d64ab327556e01
RomwelFeliciano <romfeliciano18@gmail.com>
2024-12-06 08:23:38 +0800

    laravel: comments feature

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -10,4 +10,9 @@ class Idea extends Model
     use HasFactory;
 
     protected $fillable = ['content', 'image', 'likes'];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name( 'ideas.show');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
 Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name( 'ideas.edit');
-Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name( 'ideas.update');
+Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
+Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
+
+Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.commentstore');
