e19272eecceac297ae0c47a65fe37e7b43819bec
RomwelFeliciano <romfeliciano18@gmail.com>
2024-12-06 09:20:47 +0800

    laravel: use validated data to store

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -18,7 +18,7 @@ class IdeaController extends Controller
 
         $imageName = '';
 
-        if($request->file('image')){
+        if ($request->file('image')) {
             $imageName = $this->handleImageUpload($request);
         }
 
@@ -39,9 +39,6 @@ class IdeaController extends Controller
         return redirect()->route('dashboard')->with('success', 'Idea was deleted successfully!');
     }
 
-    /**
-     * Show the edit form for an idea.
-     */
     public function edit(Idea $idea)
     {
         $editing = true;
@@ -55,13 +52,13 @@ class IdeaController extends Controller
 
         if ($request->hasFile('image')) {
             $this->deleteImage($idea->image);
-            $idea->image = $this->handleImageUpload($request);
+            $idea->update(['image' => $this->handleImageUpload($request)]);
         }
 
-        $idea->content = $validated['content'];
-        $idea->save();
+        $idea->update(['content' => $validated['content']]);
 
-        return redirect()->route('ideas.show', $idea->id)
+        return redirect()
+            ->route('ideas.show', $idea->id)
             ->with('success', 'Idea was updated successfully!');
     }
 
@@ -69,15 +66,16 @@ class IdeaController extends Controller
     {
         return [
             'content' => 'required|min:3|max:240',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ];
     }
 
     private function handleImageUpload(Request $request): ?string
     {
+        $validated = $request->validate(['image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048']);
+
         if ($request->hasFile('image')) {
-            $imageName = time() . '.' . $request->image->getClientOriginalExtension();
-            $request->image->move(public_path('images'), $imageName);
+            $imageName = time() . '.' . $validated['image']->getClientOriginalExtension();
+            $validated['image']->move(public_path('images'), $imageName);
 
             return $imageName;
         }
