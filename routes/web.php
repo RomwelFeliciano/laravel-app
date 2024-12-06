7c0b609706d0c93db5162ab89c4f1bbfbfd274a8
RomwelFeliciano <romfeliciano18@gmail.com>
2024-12-06 11:40:58 +0800

    laravel: route group

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
@@ -18,23 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store');
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])
-    ->name('ideas.edit')
-    ->middleware('auth');
-Route::put('/ideas/{idea}', [IdeaController::class, 'update'])
-    ->name('ideas.update')
-    ->middleware('auth');
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])
-    ->name('ideas.destroy')
-    ->middleware('auth');
-Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])
-    ->name('ideas.commentstore')
-    ->middleware('auth');
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'store']);
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::group(['prefix' => 'ideas', 'as' => 'ideas.'], function () {
+    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show');
+    Route::post('/', [IdeaController::class, 'store'])->name('store');
+
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+        Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
+        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('commentstore');
+    });
+});
